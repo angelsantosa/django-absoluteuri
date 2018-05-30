2ef375ad9a3e415fa0e70d0f7a3c92c2d9a50149
Angel Santos <angel_santos@outlook.com>
2018-05-30 12:42:16 -0500

    Update test_settings.py

--- test_settings.py
+++ test_settings.py
@@ -1,17 +1,29 @@
-# We just put it here to get the checks to shut up
-MIDDLEWARE_CLASSES = []
+import pkg_resources
 
-INSTALLED_APPS = (
-    'django.contrib.sites',
-    'absoluteuri',
-)
+from django.conf import settings
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-    }
-}
+try:
+    # Will be removed in Django 2.0
+    from django.core import urlresolvers
+except ImportError:
+    import django.urls as urlresolvers
 
-SITE_ID = 1
 
-ROOT_URLCONF = 'absoluteuri.tests'
+__version__ = pkg_resources.get_distribution('django-absoluteuri').version
+
+
+def build_absolute_uri(path):
+    """Turn a relative URL into an absolute URL."""
+    from django.contrib.sites.models import Site
+    site = Site.objects.get_current()
+    return '{protocol}://{domain}{path}'.format(
+        protocol=getattr(settings, 'ABSOLUTEURI_PROTOCOL', 'http'),
+        domain=site.domain,
+        path=path
+    )
+
+
+def reverse(*args, **kwargs):
+    """Reverse a URL and make it absolute."""
+    path = urlresolvers.reverse(*args, **kwargs)
+    return build_absolute_uri(path)
